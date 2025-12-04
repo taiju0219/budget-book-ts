876358c82ad737a3e86b5f2f75fbc2984c0836ec
taiju0219shukatsu-cmyk <taiju0219shukatsu@gmail.com>
2025-12-04 15:55:35 +0900

    a

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/money-tracker-ts/',
+  base: '/budget-book-ts/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
